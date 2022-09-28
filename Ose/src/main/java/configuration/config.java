90a6317eb96725692dfa50fb471a508928cdaa64
Davydov <penekmail@gmail.com>
2022-09-28 21:27:03 +0500

    up

--- Ose/src/main/java/configuration/config.java
+++ Ose/src/main/java/configuration/config.java
@@ -1,4 +1,21 @@
 package configuration;
 
+import resources.CCluster;
+import resources.CNamespace;
+
+import java.util.ArrayList;
+
 public class config {
+    String login;
+    String passwd;
+    ArrayList<CCluster> NS = new ArrayList<CCluster>();
+
+    private final configparser cfg = new configparser();
+    public String getLogin() {return login;}
+
+    public void setLogin(String login) {this.login = cfg.getLogin();}
+
+    public String getPasswd() {return passwd;}
+
+    public void setPasswd(String passwd) {this.passwd = cfg.getPasswd();}
 }
--- Ose/src/main/java/configuration/configparser.java
+++ Ose/src/main/java/configuration/configparser.java
@@ -8,7 +8,23 @@ import java.nio.file.Paths;
 
 public class configparser {
     private static final String filePath = "Ose/src/main/java/config.json";
+    private static final String content;
 
+    static {
+        try {
+            content = new String(Files.readAllBytes(Paths.get(filePath)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final JSONObject config = new JSONObject(content);
+    public String getLogin() {
+        return config.getJSONObject("User").getString("login");
+    }
+    public String getPasswd() {
+        return config.getJSONObject("User").getString("passwd");
+    }
 
     public static void main(String[] args) throws IOException {
         String content = new String(Files.readAllBytes(Paths.get(filePath)));
