7b592ca173558760219f66ef4144fc231537b9f0
Davydov <penekmail@gmail.com>
2022-09-28 19:34:58 +0500

    up

--- Ose/src/main/java/configuration/configparser.java
+++ Ose/src/main/java/configuration/configparser.java
@@ -1,6 +1,20 @@
 package configuration;
 
+import org.json.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class configparser {
-    private static final String filePath = "config.json";
+    private static final String filePath = "Ose/src/main/java/config.json";
+
+
+    public static void main(String[] args) throws IOException {
+        String content = new String(Files.readAllBytes(Paths.get(filePath)));
+        JSONObject config = new JSONObject(content);
+
+        System.out.println(config.getJSONObject("User").getString("login"));
+    }
+
 }
